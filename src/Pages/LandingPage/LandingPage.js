1be5e9446e4eeb227c8fd240b161fe5c0412dcd6
Bryce Borer <136652932+CanadianWall@users.noreply.github.com>
2024-06-18 07:54:08 -0400

    In process of fixing landing page title with logo

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,8 +1,14 @@
 import "./LandingPage.scss";
 import { Link } from "react-router-dom";
+import checkLogo from '../../assets/images/chec-logo.jpeg'
+
 const LandingPage = () => {
   return (
     <section className="scan-button-container">
+      {/* <div className="title--wrapper">
+        <h1 className="title">CHEC</h1>
+        <img className="title--logo" src={checkLogo}/>
+      </div> */}
       <Link to="/orderSummary">
         <button className="scan-button-container__button">Scan</button>
       </Link>
